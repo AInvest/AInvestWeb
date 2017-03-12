fd72eac43620cb1721acde08dc1c87178b37ad3f
JonathanSchwob <jonathanrschwob@gmail.com>
2017-03-12 14:35:50 -0700

    add ChartView

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Header from './dashboardComponents/dashboardHeader';
+import ChartView from './dashboardComponents/dashboardChart.js';
 
 export default class Dashboard extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      test: 'test',
+    };
   }
 
   render() {
     return (
       <div>
         <Header />
+        <ChartView test={this.state.test} />
       </div>
     );
   }
